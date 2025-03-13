1b7d9d39d92a15c769cbbf31c535c982d1504ce0
Exequiel <exequiel.miranda245@gmail.com>
2025-03-13 11:50:38 -0600

    crud

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 // Importo todo lo de la libreria de Express
 import express from "express";
+import productsRoutes from "./src/routes/products.js";
 
 // Creo una constante que es igual a la libreria que importé
 const app = express();
+
+//Que acepte datos en json
+app.use(express.json());
+
+// Definir las rutas de las funciones que tendrá la página web
+app.use("/api/products", productsRoutes);
 
 // Exporto la constante para poder usar express en otros archivos
 export default app;
