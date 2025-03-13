000f42fc7694a16459afe9f1fdffd074d306b17f
Jenni Teos <20210177>
2025-03-13 15:34:19 -0600

    hogrla

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 // Importo todo lo de la libreria de Express
 import express from "express";
 import productsRoutes from "./src/routes/products.js";
+import customersRoutes from "./src/routes/customers.js";
+import employeeRoutes from "./src/routes/employees.js";
+import branchesRoutes from "./src/routes/branches.js";
 
 // Creo una constante que es igual a la libreria que importé
 const app = express();
@@ -10,6 +13,9 @@ app.use(express.json());
 
 // Definir las rutas de las funciones que tendrá la página web
 app.use("/api/products", productsRoutes);
+app.use("/api/customers",customersRoutes);
+app.use("/api/employee",employeeRoutes);
+app.use("/api/branches",branchesRoutes);
 
 // Exporto la constante para poder usar express en otros archivos
 export default app;
