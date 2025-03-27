b51e857a0681f4297579507ec43ad716806c82dc
Exequiel <exequiel.miranda245@gmail.com>
2025-03-27 15:49:51 -0600

    last

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,26 @@ import productsRoutes from "./src/routes/products.js";
 import customersRoutes from "./src/routes/customers.js";
 import employeeRoutes from "./src/routes/employees.js";
 import branchesRoutes from "./src/routes/branches.js";
+import registerEmployessRoutes from "./src/routes/registerEmployees.js"
+import cookieParser from "cookie-parser"
 
 // Creo una constante que es igual a la libreria que importé
 const app = express();
 
 //Que acepte datos en json
 app.use(express.json());
+//Que postman acepte guardar cookies
+app.use(cookieParser())
 
 // Definir las rutas de las funciones que tendrá la página web
 app.use("/api/products", productsRoutes);
 app.use("/api/customers",customersRoutes);
 app.use("/api/employee",employeeRoutes);
 app.use("/api/branches",branchesRoutes);
+app.use("/api/registerEmployees", registerEmployessRoutes)
+
+
+
 
 // Exporto la constante para poder usar express en otros archivos
 export default app;
---
+++ backend/src/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+// Ejecutamos la libreria
+// para acceder al .env
+dotenv.config();
+
+export const config = {
+  db: {
+    URI: process.env.DB_URI,
+  },
+  server: {
+    port: process.env.PORT,
+  },
+  JWT: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES,
+  }
+}
